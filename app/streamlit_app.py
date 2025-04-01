7d5d2d1addee72305e0ed2519ed2f1c569a44e41
Michael Berk <michaelberkdev@gmail.com>
2025-04-01 15:56:27 -0400

    initial commit

---
+++ app/streamlit_app.py
@@ -0,0 +1,12 @@
+import streamlit as st;
+
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
+
+import numpy as np
+
+dataframe = np.random.randn(10, 20)
+st.dataframe(dataframe)
